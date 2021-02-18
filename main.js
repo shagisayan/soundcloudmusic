dc5e903ecf1901bd7aa5fd1ba836baf9ab76a1e0
Shagira Sayanthan <77782595+shagisayan@users.noreply.github.com>
2021-02-18 16:56:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ SC.initialize({
 
   $(document).ready(function(){   
       
-      SC.stream('/tracks/965789977',function(sound){ 
+      SC.stream('tracks/966127855',function(sound){ 
 
           $('#start').click(function(e) {
             e.preventDefault();
@@ -23,4 +23,4 @@ SC.initialize({
 //   <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" 
 //   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/965789977&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/gunda-siddanth" title="XD" target="_blank" style="color: #cccccc; text-decoration: none;">XD</a> · <a href="https://soundcloud.com/gunda-siddanth/master-the-blaster" title="Master The Blaster" target="_blank" style="color: #cccccc; text-decoration: none;">Master The Blaster</a></div>
 
-  
+ 
